ae13399a1585b862f2272339a8498bb9d388f67d
MrBreOr <egor.bredzikhin@gmail.com>
2022-02-20 21:38:24 +0300

    Добавьте файлы проекта.

---
+++ testerProg/Body.cs
@@ -0,0 +1,93 @@
+﻿namespace testerProg
+{
+    internal class Body
+    {
+        public Dictionary<string, int> costs = new() { { End, int.MaxValue } };
+        public Dictionary<string, string> perents = new();
+
+        public Dictionary<string, Dictionary<string, int>> graph = new() { { Start, new() } };
+
+
+        public const string Start = "start";
+        public const string End = "end";
+
+        public Body(params string[] list)
+        {
+            foreach (string l in list)
+            {
+                graph[l] = new();
+                costs[l] = int.MaxValue;
+            }
+        }
+
+        public void Add(params (string,string,int)[] param)
+        {
+            foreach (var p in param)
+            {
+                graph[p.Item1][p.Item2] = p.Item3;
+                perents[p.Item2] = p.Item1;
+            }
+
+            foreach (var name in graph[Start].Keys)
+            {
+                costs[name] = graph[Start][name];
+            }
+        }
+
+
+
+        public void Processed(string startName = Start)
+        {
+            List<string> allOff = new() { Start, End };
+
+            while (allOff.Count != costs.Count)  
+            {
+                string name = FindMinCach(allOff);
+                Dictionary<string, int> node = graph[name];
+                foreach (var n in node.Keys)
+                {
+                    int cost = costs[name];
+                    int new_costs = cost + node[n];
+
+                    if (costs[n] > new_costs)
+                    {
+                        costs[n] = new_costs;
+                        perents[n] = name;
+                    }
+                }
+                allOff.Add(name);
+            }
+        }
+
+        private string FindMinCach(List<string> names)
+        {
+            int minCach = int.MaxValue;
+            string nameMin = "";
+            foreach (var name in costs.Keys)
+            {
+                if (Serch(names, name)) continue;
+
+                if(minCach > costs[name])
+                {
+                    minCach = costs[name];
+                    nameMin = name;
+                }
+            }
+            return nameMin;
+        }
+
+        private bool Serch(List<string> allOff, string name)
+        {
+            foreach(var a in allOff)
+            {
+                if (name == a) return true;
+            }
+            return false;
+        }
+
+        public int MinPath()
+        {
+            return costs[End];
+        }
+    }
+}
---
+++ testerProg/Program.cs
@@ -0,0 +1,23 @@
+﻿Body body = new Body("a", "b", "c", "d");
+body.Add((Body.Start, "a", 2),
+         (Body.Start, "b", 2),
+         ("b", "a", 2),
+         ("a", Body.End, 2),
+         ("a", "c", 2),
+         ("c", "b", -1),
+         ("c", Body.End, 2));
+
+body.Processed();
+
+Console.WriteLine(body.MinPath());
+
+Console.ReadKey();
+
+
+//List<Body> testTree = new List<Body>(6);
+//testTree.Add(new Body("start", new Dictionary<string, int> { { "A", 5 }, { "B", 2 } }));
+//testTree.Add(new Body("A", new Dictionary<string, int> { { "",  }, { "",  } }));
+//testTree.Add(new Body("B", new Dictionary<string, int> { { "",  }, { "",  } }));
+//testTree.Add(new Body("C", new Dictionary<string, int> { { "",  }, { "",  } }));
+//testTree.Add(new Body("D", new Dictionary<string, int> { { "",  }, { "",  } }));
+//testTree.Add(new Body("end", new Dictionary<string, int> { { "",  }, { "",  } }));
